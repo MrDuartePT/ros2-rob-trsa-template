95b017ed9a34b7d6790dfdfac542c7c99709decb
Gonçalo Negrier Duarte <gonegrier.duarte@gmail.com>
2024-10-07 13:55:22 +0100

    Install Ros Desktop Full package instead * Update readme * add ./rob_ws and ./trsa_ws mountpoint instead * add missing packages
    Signed-off-by: Gonçalo Negrier Duarte <gonegrier.duarte@gmail.com>

--- .devcontainer/hooks/postCreate.sh
+++ .devcontainer/hooks/postCreate.sh
@@ -3,12 +3,14 @@
 # It can be used for setup steps outside the Dockerfile.
 
 . /opt/ros/$ROS_DISTRO/setup.sh
+. /usr/share/gazebo/setup.bash
 
 # Auto-activate ROS whenever bash shell is opened.
 echo "source /opt/ros/$ROS_DISTRO/setup.bash\nsource $WORKSPACE_ROOT/install/local_setup.bash" >> ~/.bashrc
 
-# (OPTION) Symlink `/data` mount point to workspace folder for convenience.
-# ln -sf /data "$WORKSPACE_ROOT/"
+# Symlink `./rob_ws` and `./trsa_ws` mount point to workspace folder for convenience.
+ln -sf /home/vscode/rob_ws "$WORKSPACE_ROOT/"
+ln -sf /home/vscode/trsa_ws "$WORKSPACE_ROOT/"
 
 # Something deleted the package indexes so we re-download them for convenience.
 apt-get update
