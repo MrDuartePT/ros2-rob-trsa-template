e423c79b9de447238e88b2d2658738425e2d53af
Gonçalo Negrier Duarte <gonegrier.duarte@gmail.com>
2024-10-26 00:37:49 +0100

    change vnc resolution to 1080p (use fullscreen)
    Signed-off-by: Gonçalo Negrier Duarte <gonegrier.duarte@gmail.com>

--- .devcontainer/scripts/desktop-lite-debian.sh
+++ .devcontainer/scripts/desktop-lite-debian.sh
@@ -226,7 +226,7 @@ LOG=/tmp/container-init.log
 
 export DBUS_SESSION_BUS_ADDRESS="${DBUS_SESSION_BUS_ADDRESS:-"autolaunch:"}"
 export DISPLAY="${DISPLAY:-:1}"
-export VNC_RESOLUTION="${VNC_RESOLUTION:-1440x768x16}" 
+export VNC_RESOLUTION="${VNC_RESOLUTION:-1920x1080x16}" 
 export LANG="${LANG:-"en_US.UTF-8"}"
 export LANGUAGE="${LANGUAGE:-"en_US.UTF-8"}"
 
