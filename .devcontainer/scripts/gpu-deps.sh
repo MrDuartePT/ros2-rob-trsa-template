7058abb6e47b6f1dc85a2975e5efae35ef0f5f85
Gonçalo Negrier Duarte <gonegrier.duarte@gmail.com>
2025-10-27 12:57:44 +0000

    gpu-deps: reduce nvidia packages to minimal runtime
    Signed-off-by: Gonçalo Negrier Duarte <gonegrier.duarte@gmail.com>

--- .devcontainer/scripts/gpu-deps.sh
+++ .devcontainer/scripts/gpu-deps.sh
@@ -3,21 +3,21 @@
 apt-get update && apt-get install -y ocl-icd-libopencl1 clinfo
 mkdir -p /etc/OpenCL/vendors
 
-# Intel GPU/NPU depedencies
+# Intel GPU/NPU depedencies (478MB)
 curl -fsSL https://repositories.intel.com/gpu/intel-graphics.key | gpg --dearmor -o /usr/share/keyrings/intel-graphics.gpg && \
-echo "deb [arch=amd64 signed-by=/usr/share/keyrings/intel-graphics.gpg] https://repositories.intel.com/gpu/ubuntu jammy/lts/2350 unified" > /etc/apt/sources.list.d/intel-gpu-jammy.list
+echo "deb [arch=amd64 signed-by=/usr/share/keyrings/intel-graphics.gpg] https://repositories.intel.com/gpu/ubuntu jammy unified" > /etc/apt/sources.list.d/intel-gpu-jammy.list
 
 apt-get update && apt-get install -y intel-opencl-icd intel-media-va-driver-non-free mesa-va-drivers \
-    mesa-vdpau-drivers mesa-vulkan-drivers va-driver-all level-zero intel-level-zero-gpu \
+    mesa-vdpau-drivers mesa-vulkan-drivers va-driver-all \
     libze-intel-gpu1 libze1 intel-opencl-icd libze-dev intel-ocloc clinfo
 echo "libintelocl.so" > /etc/OpenCL/vendors/intel.icd
 
-# AMD GPU/NPU depedencies
+# AMD GPU/NPU depedencies (661MB)
 curl -fsSL https://repo.radeon.com/rocm/rocm.gpg.key | gpg --dearmor -o /usr/share/keyrings/rocm.gpg && \
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/rocm.gpg] https://repo.radeon.com/rocm/apt/6.4/ $(. /etc/os-release && echo $UBUNTU_CODENAME) main" > /etc/apt/sources.list.d/rocm.list && \
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/rocm.gpg] https://repo.radeon.com/amdgpu/6.4/ubuntu $(. /etc/os-release && echo $UBUNTU_CODENAME) main" > /etc/apt/sources.list.d/amdgpu.list;
 
-apt-get update && apt-get install -y --no-install-recommends rocm-opencl-runtime libnuma1 libtinfo6 libnvvpi3
+apt-get update && apt-get update && apt-get install -y rocm-opencl-runtime rocm-language-runtime
 echo "libamdocl64.so" > /etc/OpenCL/vendors/amdocl64.icd
 
 # NVIDIA depedencies (CUDA 12.9)
@@ -27,25 +27,15 @@ echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/jetso
 wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/x86_64/cuda-ubuntu2204.pin && sudo mv cuda-ubuntu2204.pin /etc/apt/preferences.d/cuda-repository-pin-600
 curl -fsSLO https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/x86_64/cuda-keyring_1.1-1_all.deb && dpkg -i cuda-keyring_1.1-1_all.deb
 
-# Runtime + nvcc compiler
-apt-get update && apt-get install -y \
-    cuda-compiler-12-9 \
-    cuda-cudart-dev-12-9 \
-    cuda-command-line-tools-12-9 \
-    cuda-minimal-build-12-9 \
-    cuda-compat-12-9 \
-    cuda-libraries-dev-12-9 \
-    cuda-nvml-dev-12-9 \
-    cuda-nvprof-12-9 \
-    libnpp-dev-12-9 \
+# Minimal Runtime (4205 MB)
+apt-get update && apt-get install -y --no-install-recommends \
+    cuda-libraries-12-9 \
+    libcudnn9-cuda-12 \
     cuda-nvtx-12-9 \
     libcusparse-12-9 \
-    libcublas-dev-12-9 \
-    libnccl-dev \
-    libcudnn9-cuda-12 \
-    libcudnn9-dev-cuda-12 \
-    cuda-toolkit-12-9 \
-    #cuda-nsight-compute-12-9 (This 8gb in size install locally if needed)
+    libnpp-12-9 \
+    libcublas-12-9 \
+    libnccl2
 
 echo "/usr/local/cuda/lib64" >> /etc/ld.so.conf.d/nvidia.conf
 echo 'export PATH=/usr/local/cuda/bin:$PATH' >> /etc/profile.d/cuda.sh
